c9aa45846463bac082dd985162ee3a90bcd3640d
sendjin5 <sendjin5@gmail.com>
2025-01-06 17:27:50 +0900

    react 연습

--- calc/src/App.js
+++ calc/src/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { Button } from "react-native-web";
+import Button from "./components/Button";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={[styles.error, styles.text]}>React Native Start</Text>
-      <Button title="실행" onPress={{}} color="red" />
+      <Button
+        title="실행"
+        color={"red"}
+        onPress={() => console.log("콘솔로그 클릭")}
+      />
       <StatusBar style="auto" />
     </View>
   );
