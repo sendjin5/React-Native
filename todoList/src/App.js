afb37ba51f0efaa61b70b3dd1a6f86eba2db643e
sendjin5 <sendjin5@gmail.com>
2025-01-16 16:35:47 +0900

    App.js move file

---
+++ todoList/src/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    // eslint-disable-next-line react/react-in-jsx-scope
+    <View style={styles.container}>
+      <Text style={{ fontSize: 50 }}>TodoList App Start</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
