fb4f1f381a4eaf8dc1d5745399539caec3798bd3
sendjin5 <sendjin5@gmail.com>
2025-01-17 10:09:58 +0900

    eslint update

--- todoList/eslint.config.mjs
+++ todoList/eslint.config.mjs
@@ -6,7 +6,7 @@ import pluginReact from "eslint-plugin-react";
 export default [
   { files: ["**/*.{js,mjs,cjs,jsx}"] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
+  // pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.recommended,
 ];
