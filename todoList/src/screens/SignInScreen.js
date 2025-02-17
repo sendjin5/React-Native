fe0352831722bf5598235900753351a9ffea767a
sendjin5 <sendjin5@gmail.com>
2025-02-17 14:05:34 +0900

    todoList update

---
+++ todoList/src/screens/SignInScreen.js
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+
+const SignInScreens = () => {
+  return <View style={Styles.contatiner}></View>;
+};
+
+const Styles = StyleSheet.create({
+  contatiner: {
+    flex: 1,
+    justifyContents: "center",
+  },
+});
